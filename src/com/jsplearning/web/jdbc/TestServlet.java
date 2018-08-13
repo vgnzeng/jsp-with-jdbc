ac3e1a0584bdb637b7bb2fae65005f9fa206b871
Guannian Zeng <vgnzeng@gmail.com>
2018-08-13 11:28:33 -0700

    updated the testing Servlet for Tomcat Connection Pool

--- src/com/jsplearning/web/jdbc/TestServlet.java
+++ src/com/jsplearning/web/jdbc/TestServlet.java
@@ -1,11 +1,18 @@
 package com.jsplearning.web.jdbc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class TestServlet
@@ -13,29 +20,53 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
+	// Define datasource/connection pool for Resource Injection
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// Step 1:  Set up the printwriter
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		
+		// Step 2:  Get a connection to the database
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			myConn = dataSource.getConnection();
+			
+			// Step 3:  Create a SQL statements
+			String sql = "select * from student";
+			myStmt = myConn.createStatement();
+			
+			// Step 4:  Execute SQL query
+			myRs = myStmt.executeQuery(sql);
+			
+			// Step 5:  Process the result set
+			while (myRs.next()) {
+				String email = myRs.getString("email");
+				String lastName = myRs.getString("last_name");
+				out.println(lastName + " - " + email);
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 
 }
+
+
+
+
+
+
+

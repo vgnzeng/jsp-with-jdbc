bc8ffa16040bd6f8117dfad4c4d338d8ffc0bb45
Guannian Zeng <vgnzeng@gmail.com>
2018-08-13 14:12:55 -0700

    updated add-stuendt function on database helper class

--- src/com/jsplearning/web/jdbc/StudentDbUtil.java
+++ src/com/jsplearning/web/jdbc/StudentDbUtil.java
@@ -81,9 +81,34 @@ public class StudentDbUtil {
 		}
 	}
 
-	public void addStudent(Student theStudent) {
-		// TODO Auto-generated method stub
+	public void addStudent(Student theStudent) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
 		
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+			
+			// create sql for insert
+			String sql = "insert into student "
+					   + "(first_name, last_name, email) "
+					   + "values (?, ?, ?)";
+			
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set the param values for the student
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			
+			// execute sql insert
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}
 	}
 
 }
